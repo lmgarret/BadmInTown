7c0aa94dc4c80c50b960121b0c045778d96e844f
LM. Garret <louis-maxence.garret@epfl.ch>
2018-10-31 12:12:05 +0100

    Added file to init db

---
+++ data/init_db.sql
@@ -0,0 +1,63 @@
+CREATE TABLE Tournament (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text,
+	start_date date,
+	url text,
+	location_id integer
+);
+
+CREATE TABLE Location (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	latitude float,
+	longitude float
+);
+
+CREATE TABLE TournamentRanking (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text,
+	short_name text
+);
+
+CREATE TABLE TournamentCategory (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text,
+	short_name text
+);
+
+CREATE TABLE Club (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text,
+	short_name text,
+	location_id integer
+);
+
+CREATE TABLE Player (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text,
+	surname text,
+	gender binary
+);
+
+CREATE TABLE organizes (
+	tid integer,
+	cid integer,
+	PRIMARY KEY(tid,cid)
+);
+
+CREATE TABLE accepts_ranking (
+	tid integer,
+	trid integer,
+	PRIMARY KEY(tid,trid)
+);
+
+CREATE TABLE accept_category (
+	tid integer,
+	tcid integer,
+	PRIMARY KEY(tid,tcid)
+);
+
+CREATE TABLE participates (
+	pid integer,
+	tid integer,
+	PRIMARY KEY(pid,tid)
+);
